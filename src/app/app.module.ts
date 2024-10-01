738dc8691493724a7bcf1ea22612c278264a44d2
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-10-01 19:21:38 +0545

    feat: add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { IncomeExpenseFormComponent } from './components/income-expense-form/income-expense-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegistrationComponent,
+    IncomeExpenseFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/income-expense-form/income-expense-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-income-expense-form',
+  templateUrl: './income-expense-form.component.html',
+  styleUrl: './income-expense-form.component.scss'
+})
+export class IncomeExpenseFormComponent {
+
+}
