5e4c8a8cd0a3c3a7b5df2ef9131cb26455a90481
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-10-02 15:42:41 +0545

    feat: add login user functionality and validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../core/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,25 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
+  error:  string = "'Username or password invalid'";
 
+
+  constructor(private formBuilder:  FormBuilder,private authService:AuthService,private router:Router) {
+    this.loginForm = this.formBuilder.group({
+      email:['',[Validators.required,Validators.email]],
+      password:['',[Validators.required]]
+    })
+  }
+
+  login():void{
+    if(this.loginForm.valid){
+      const {email,password} = this.loginForm.value;
+      if (this.authService.login(email, password)) {
+        this.router.navigate(['/dashboard']);
+      } else {
+        alert('Login failed');
+      }
+    }
+  }
 }
