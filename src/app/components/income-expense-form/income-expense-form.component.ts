a31c92f9108753e44b614e2e3ef3d8790f5dccff
Manohar shrestha <nov29sthamanohar@gmail.com>
2024-10-04 14:39:39 +0545

    feat: add income-expense

--- src/app/components/income-expense-form/income-expense-form.component.ts
+++ src/app/components/income-expense-form/income-expense-form.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { IncomeExpense } from '../../models/income-expense.model';
+import { IncomeExpenseService } from '../../core/income-expense.service';
 
 @Component({
   selector: 'app-income-expense-form',
   templateUrl: './income-expense-form.component.html',
-  styleUrl: './income-expense-form.component.scss'
+  styleUrl: './income-expense-form.component.scss',
 })
 export class IncomeExpenseFormComponent {
+  incomeExpenseForm: FormGroup;
+  isEditMode = false;
+  submitText: string = '';
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private incomeExpenseService: IncomeExpenseService,
+    private dialogRef: MatDialogRef<IncomeExpenseFormComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { entry?: IncomeExpense } // Inject data
+  ) {
+    this.incomeExpenseForm = this.formBuilder.group({
+      name: ['', [Validators.required]],
+      amount: [null, [Validators.required, Validators.min(1)]],
+      date: ['', [Validators.required]],
+      type: ['income', [Validators.required]],
+      recurring: ['one time', [Validators.required]],
+      id: [null],
+    });
+
+    if (data && data.entry) {
+      this.isEditMode = true;
+      this.incomeExpenseForm.patchValue(data.entry);
+    }
+    this.submitText = this.isEditMode ? 'Update' : 'Create';
+  }
+
+  saveEntry(): void {
+    if (this.incomeExpenseForm.valid) {
+      if (this.isEditMode) {
+        this.incomeExpenseService.updateEntry(this.incomeExpenseForm.value); 
+      } else {
+        this.incomeExpenseService.addEntry(this.incomeExpenseForm.value); 
+      }
+      this.closeDialog();
+    } else {
+      alert('Please fill all fields correctly');
+    }
+  }
+
+  closeDialog(): void {
+    this.dialogRef.close();
+  }
 }
